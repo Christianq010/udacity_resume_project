59f771c2c04c218cd3bc80e31c72875035add5f2
Christiaan Quyn <christianq010@gmai.com>
2016-11-16 22:29:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Project Details
+Update a resume with new skills and projects as you progress along the Nanodegree Program.
+
+1. All of the HTML code needed to build the resume is stored in **js/helper.js** variables. The variable names indicate their function. 
+2. All Styling of these elements has been changed to suit the design but the data in the javaScript objects are appended or prepended into the appropriate location.
+
+Repository includes the following files:
+
+* **assets(dir)**: Contains all of the fonts, CSS, JS and other resources that make up the Custom Bootstrap Theme.
+* **bootstrap3**: Contains all Bootstrap 3 Resources.
+* **index.html**: The main HTML document. Contains links to all of the CSS and JS resources needed to render the resume, including resumeBuilder.js.
+* **js/helper.js**: Contains helper code needed to format the resume and build the map. Styling of certain elements has been changed to suit the design.
+* **js/resumeBuilder.js**: Data File.
+* **js/jQuery.js**: The jQuery library.
+* **css/custom.css**: Contains all my custom styles added to the Framework.
+* **README.md**: The GitHub readme file.
+* and some images in the images directory.
+
+### My process:
+1. Re-designed the entire Resume with a custom Bootstrap Framework by Creative Tim(http://demos.creative-tim.com/paper-kit/).
+2. Build four JavaScript objects, each one representing a different resume section.
+3. Iterate through each javaScript object and append its information to index.html in different sections.
+4. All Project Styling has been changed to suit the Custom Theme.
+
+* Responsive layout for different devices not done yet.
+
+##Hosted via Github Pages - https://christianq010.github.io/nanodegree_resume_Project/
---
+++ js/resumeBuilder.js
@@ -0,0 +1,222 @@
+/*
+Data Objects.
+ */
+
+var bio = {
+        "name" : "Christiaan Quyn",
+        "role" : "Front-End Developer",
+        "contacts" : {
+            "mobile" : "+94 777549947",
+            "email": "christianq010@gmail.com",
+            "github": "https://github.com/Christianq010",
+            "linkedIn" : "https://lk.linkedin.com/in/christianq010",
+            "blog" : "https://medium.com/@christianq010",
+            "location": "Colombo, Sri Lanka"
+        },
+        "welcomeMessage": "Hi, I started programming while working as an HVAC Tech - pursuing my Engineering degree. I'm incredibly passionate about developing software and creating interesting user experiences over the web.",
+        "skills": ["HTML5", "CSS3", "Javascript","Sass", "Bootstrap", "jQuery", "Git"],
+        "bioPic": "./assets/paper_img/sw-pic1.jpg"
+}
+
+var education = {
+    "schools" : [
+        {
+            "name" : "University of Wolverhampton",
+            "location" : "BCAS City Campus, Colombo",
+            "degree" : "BEng (Hons)",
+            "majors" : ["Telecommunication Engineering", "Electronic Engineering"],
+            "dates" : "2016-2019",
+            "courses" : ["BTEC Diploma in Engineering Foundations", "HND in Telecom Eng"],
+            "url" : "http://courses.wlv.ac.uk/course.asp?code=MA006H31UVD"
+        }
+    ],
+    "onlineCourses" : [
+        {
+            "title" : "Udacity Front-End Developer Nanodegree",
+            "school" : "Udacity Inc.",
+            "dates" : "2016",
+            "url" : "https://www.udacity.com"
+        },
+        {
+            "title" : "Treehouse Front-End Developer Track",
+            "school" : "Treehouse Island Inc.",
+            "dates" : "2015-2016",
+            "url" : "https://teamtreehouse.com"
+        }
+    ]
+}
+
+var work = {
+    "jobs": [
+        {
+            "employer": "Lanka Energy Conservation",
+            "title": "Trainee Engineer",
+            "description": "Trainee Mechanical Service Engineer / Tech for HVAC Equipment",
+            "dates" : "2015-present",
+            "location": "Colombo 4, Sri Lanka"
+        },
+        {
+            "employer": "Cinnamon Grand Hotel",
+            "title": "Engineering Apprentice",
+            "description": "Engineering Maintenance Technician for a variety of equipment including HVAC, Electrical and Mechanical",
+            "dates" : "2014-2015",
+            "location": "Colombo 3, Sri Lanka"
+        }
+    ]
+}
+
+var projects = {
+    "projects": [
+        {
+            "title": "SMARDT-Powerpax Training at Melbourne Factory",
+            "dates": "Aug 2015",
+            "description": "Certification for Servicing of SMARDT Equipment",
+            "images": ["./assets/paper_img/smardt_globe.png"]
+        },
+        {
+            "title": "SMARDT Installations",
+            "dates": "2015-2016",
+            "description": "Installation of various SMARDT Chillers across the country",
+            "images": ["./assets/paper_img/smardt-chiller.jpg"]
+        }
+    ]
+}
+
+bio.display = function () {
+
+    //Variables for all bio details
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var formattedContactMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedContactEmail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedlinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkedIn);
+    var formattedBlog = HTMLblog.replace("%data%", bio.contacts.blog);
+    var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+    var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+    //Append variables onto the Page
+    $(".name").prepend(formattedName);
+    $(".role").prepend(formattedRole);
+    $(".avatar").append(formattedBioPic);
+    $(".skillsStart").append(HTMLskillsStart);
+    $(".message").append(formattedWelcomeMsg);
+
+    //Append Contact Info to Top of the page
+    $(".contactMobile").append(formattedContactMobile);
+    $(".contactMail").append(formattedContactEmail);
+    $(".contactGithub").append(formattedGithub);
+    $(".contactLinkedIn").append(formattedlinkedin);
+    $(".contactLocation").append(formattedLocation);
+
+    //Loop Through each Skill
+    bio.skills.forEach(function (skill) {
+        var formattedSkill = HTMLskills.replace("%data%",skill);
+        $("#skills").append(formattedSkill);
+    });
+
+    //Contact details for the footer
+    $(".footerMobile").append(formattedContactMobile);
+    $(".footerEmail").append(formattedContactEmail);
+    $(".footerLocation").append(formattedLocation);
+    $(".footerBlog").append(formattedBlog);
+};
+
+
+$("#education").append(HTMLschoolStart);
+
+education.display = function () {
+
+    education.schools.forEach(function(school) {
+
+        var formattedSchoolName = HTMLschoolName.replace("%data%", school.name);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", school.degree);
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%", school.dates);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
+        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", school.majors);
+
+        $(".education-entry:last").append(formattedSchoolName, formattedDegree, formattedSchoolDates, formattedSchoolLocation, formattedSchoolMajor);
+    });
+        //repeated so Online Courses appears Below Main Education
+        $("#education").append(HTMLschoolStart);
+        //append Online Courses in Education section
+        $("#education").append(HTMLonlineClasses);
+
+    education.onlineCourses.forEach(function(onlineCourse) {
+
+        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", onlineCourse.title);
+        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", onlineCourse.school);
+        var formattedOnlineCourseDates = HTMLonlineDates.replace("%data%", onlineCourse.dates);
+        var formattedOnlineUrl = HTMLonlineURL.replace("%data%", onlineCourse.url);
+
+        $(".education-online").append(formattedOnlineTitle, formattedOnlineSchool, formattedOnlineCourseDates, formattedOnlineUrl);
+    });
+
+};
+
+
+work.display = function () {
+
+    work.jobs.forEach(function (job) {
+
+        //create a new div for work section
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
+
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDates = HTMLworkDates.replace("%data%", job.dates);
+        var formattedLocation = HTMLworkLocation.replace("%data%", job.location);
+        var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
+
+        $(".work-entry:last").append(formattedDates, formattedLocation, formattedDescription);
+    });
+};
+
+projects.display = function () {
+
+    projects.projects.forEach(function(project) {
+        //new Project Section
+        $("#projects").append(HTMLprojectStart);
+
+        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", project.title);
+        var formattedProjectDates = HTMLprojectDates.replace("%data%", project.dates);
+        var formattedProjectDescription = HTMLprojectDescription.replace("%data%", project.description);
+
+        $(".project-entry:last").append(formattedProjectTitle, formattedProjectDates, formattedProjectDescription);
+
+        //Loop Through Each Image
+        project.images.forEach(function (image) {
+            var formattedProjectImage = HTMLprojectImage.replace("%data%", image);
+            $(".project-entry:last").append(formattedProjectImage);
+        });
+    });
+};
+
+    //International Name
+    function inName(name) {
+        name = name.trim().split(" ");
+        console.log(name);
+
+        name[1] = name[1].toUpperCase();
+        name[0] = name[0].slice(0, 1).toUpperCase() +
+            name[0].slice(1).toLowerCase();
+
+        var intName = name[0] + " " + name[1];
+
+        return intName();
+    }
+    $(".internationalize-btn").append(internationalizeButton);
+
+//Call all display functions to display on Page
+bio.display();
+education.display();
+work.display();
+projects.display();
+
+    //Google Maps
+    $("#mapDiv").append(googleMap);
